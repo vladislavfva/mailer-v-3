963d5acaadd4e791e121b3fe97a26a6f67dc4ad9
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-05-12 13:16:26 +0200

    s

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 443;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
